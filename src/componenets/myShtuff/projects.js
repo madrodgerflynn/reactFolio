3d173afa59523ce82cdfe7ad7af3c3a58d0986b8
madrodgerflynn <madrodgerflynn@gmail.com>
2022-10-21 12:40:59 -0600

    initial commit

---
+++ src/componenets/myShtuff/projects.js
@@ -0,0 +1,27 @@
+const projects = [
+  {
+    index: 0,
+    alt: "Bubble Dungeonz is a game hosting platform that includes user authentication and a chat feature.",
+    title: "Bubble DungeonZ",
+    description:
+      "A full stack MERN application for hosting simple online games and live chat.",
+    appLink: "https://bubbledungeonz.herokuapp.com/",
+    gitHub: "https://github.com/l1keafox/bubbleDungeonz",
+  },
+  {
+    index: 1,
+    alt: "The Kiss the Cook app features a title header and navigation bar aside. The screenshot shows the All Recipes page which includes clickable recipe cards with embedded images and titles.",
+    title: "Kiss the Cook",
+    description:
+      "A full stack recipe application for the blog-averse home cook. Featuring a MySQL database, ExpressJS server, NodeJS front end with the Handlebars template engine.",
+    appLink: "https://kiss-the-cook.herokuapp.com/",
+    gitHub: "https://github.com/madrodgerflynn/Kiss_The_Cook",
+  },
+  {
+    index: 3,
+    alt: "An application for users to track firends, their thoughts and reactions.",
+    title: "The API Social Network",
+  },
+];
+
+export default projects;
