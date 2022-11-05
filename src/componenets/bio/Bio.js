56bfaf2161397197f8324bca0f45c75076832594
madrodgerflynn <madrodgerflynn@gmail.com>
2022-11-05 10:55:46 -0600

    didn't save last night

--- src/componenets/bio/Bio.js
+++ src/componenets/bio/Bio.js
@@ -8,41 +8,43 @@ export default function Bio() {
     <div className="stuffContainer">
       <div className="bioContainer">
         <h1>Welcome to my portfolio!</h1>
-        <TypeIt
+        {/* <TypeIt
           className="textWindow"
           options={{
             speed: 10,
             waitUntilVisible: true,
           }}
           // element={"p"}
-        >
-          My name is Tyler Anderson. I have always had an interest in all things
-          computer related. I attempted to study computer science once I was in
-          college, but got distracted by another subject, and graduated with a
-          BS in Anthropology. I then attended a technical university and
-          received an associates in Electrical Engineering. Again I was given
-          the chance to code and really enjoyed it. It wasn't until many years
-          and a couple jobs later that I was given another oppourtunity to code.
-          I jumped into it head on with the DU Cooding Bootcamp, and haven't
-          looked back
-        </TypeIt>
+        ></TypeIt> */}
+
+        <TypeIt
+          options={{
+            speed: 10,
+          }}
+          getBeforeInit={(instance) => {
+            instance
+
+              .pause(500)
+              .type(
+                "My name is Tyler Anderson. I have always had an interest in all things computer related. I attempted to study computer science once I was in college, but got distracted by another subject, and graduated with a BS in Anthropology. I then attended a technical university and received an associates in Electrical Engineering. Again I was given the chance to code and really enjoyed it. It wasn't until many years and a couple jobs later that I was given another oppourtunity to code. I jumped into it head on with the DU Cooding Bootcamp, and haven't looked back"
+              )
+              .pause(500)
+              .break()
+              .pause(200)
+              .break()
+              .type(
+                "After the bootcamp I have continued to code everyday and enjoyed every minute! Coding is another interactive outlet that allows me to be creative!"
+              )
+              .break()
+              .pause(500)
+              .break()
+              .type(
+                "My other job is a metalsmith. I design and create jewlery, mainly with scraps I find."
+              );
+            return instance;
+          }}
+        ></TypeIt>
       </div>
-      {/* <TypeIt
-            getBeforeInit={(instance) => {
-              instance
-                .pause(500)
-                .type("After the bootcamp I have continued to code everyday and enjoyed every minute! Coding is another interactive outlet that allows me to be creative!")
-                .break()
-                .pause(500)
-                .break()
-                .pause(1500)
-                .type("My "other job is a metalsmith. I design")
-                .break()
-                .pause(700)
-                .type("Want to see it?");
-              return instance;
-         
-          ></TypeIt> */}
       <Footer />
     </div>
   );
