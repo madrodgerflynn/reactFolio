1edfa3e694d381a259f2255c7dc7c097ecf40eae
madrodgerflynn <madrodgerflynn@gmail.com>
2022-11-09 14:50:44 -0700

    mostly done, still having some response issues

--- src/componenets/sideBar/SideBar.js
+++ src/componenets/sideBar/SideBar.js
@@ -4,7 +4,7 @@ import { GoMarkGithub } from "react-icons/go";
 function SideBar() {
   return (
     <div className="sideBarContainer">
-      <i className="GoMarkGithub"></i>
+      <i className={GoMarkGithub}></i>
       <i className="phoneNumber"></i>
       <i className=""></i>
     </div>
